9c6770b238e20cb02da08349aa7b7376e6d5c77e
Mohd Aaqib Hamdule <aaqibhamdule@yahoo.com>
2025-08-18 00:40:58 +0530

    Revert "Add initial product and category API layer"

--- src/main/java/com/ec2demo/ec2demo/catalog/repository/ProductCategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ec2demo.ec2demo.catalog.repository;
-
-import com.ec2demo.ec2demo.catalog.entity.ProductCategory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface ProductCategoryRepository extends JpaRepository<ProductCategory, UUID> {
-}
